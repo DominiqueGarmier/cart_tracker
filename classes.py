1584adae125641852007ad784e5a5a7bee5f9766
Dominique <dominique@garmier.ch>
2020-08-10 15:42:54 +0200

    IO

--- classes.py
+++ classes.py
@@ -4,7 +4,7 @@ import re
 # local imports
 
 # 3rd party imports
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
 
 class IOCache:
@@ -15,8 +15,9 @@ class IOCache:
 
 class IOHandler:
     
-    def __init__(self):
-        self.cache = IOCache()
+    def __init__(self, data):
+        self._cache = IOCache()
+        self._data = data
 
     def ask_for_input(self):
         '''
@@ -30,12 +31,12 @@ class IOHandler:
         stores input as class variable of self.cache
         '''
 
-        self.cache.raw_numbers = input("\n -> Welche(r) Wagen haben Sie beladen?\n")
-        self.cache.raw_signature = input("\n -> Was sind Ihre Initialen?\n")
+        self._cache.raw_numbers = input("\n -> Welche(r) Wagen haben Sie beladen?\n")
+        self._cache.raw_signature = input("\n -> Was sind Ihre Initialen?\n")
 
     def process_input(self):
         '''
-        args. None
+        args: None
 
         returns: list of NewEntry Objects
         
@@ -43,26 +44,27 @@ class IOHandler:
         '''
 
         # process cart_numbers
-        cart_numbers = re.split(',|, |;|; ', self.cache.raw_numbers)
+        cart_numbers = re.split(',|, |;|; ', self._cache.raw_numbers)
 
         # process signatue
-        signatrue = self.cache.raw_signature
+        signatrue = self._cache.raw_signature
 
         recent_entries = []
         for cart_number in cart_numbers:
             _ = Entry(cart_number=cart_number, state='done', signatrue=signatrue)
             recent_entries.append(_)
 
-        self.cache.recent_entries = recent_entries
+        self._cache.recent_entries = recent_entries
 
     def print_recent_entries(self):
-        for entry in self.cache.recent_entries:
+        for entry in self._cache.recent_entries:
             entry.print_entry()
-
 
 class Entry:
 
     def __init__(self, cart_number=None, state=None, signatrue=None):
+        '''
+        '''
         self._cart_number = cart_number # string
         self._state = state             # string: "done" or "pending" #TODO use bool?
         self._signature = signatrue       # string
@@ -74,3 +76,26 @@ class Entry:
         print('Cart Number: ', self._cart_number, end=' | ')
         print('State: ', self._state, end=' | ')
         print('Signature: ', self._signature, end='\n')
+
+
+class Data:
+    
+    def __init__(self, file_path):
+        self._file_path = file_path
+    
+    def pull(self):
+        '''
+        updates pandas df by pulling from xlsx file
+        '''
+        pass
+
+    def push(self):
+        '''
+        saves pandas df as xlsx file
+        '''
+        pass
+
+    def edit(self):
+        '''
+        edits contents of df
+        '''
