00c0aef1ea8ce9b4d534db9bec67458aef5374af
Flotzdrue <flotzflotz6@gmail.com>
2020-08-19 07:40:44 +0200

    added for specific ps command to pull repo

--- update.py
+++ update.py
@@ -3,6 +3,7 @@ import subprocess
 
 # try to pull from git repo
 try:
-    subprocess.call('Powershell -command "git pull"')
+    # this pulls the maser branch from origin
+    subprocess.call('Powershell -command "git pull origin master"')
 except:
     pass
